df478a177e7999883595e3e11ab4500071097c29
misostack <gmmiso88@gmail.com>
2025-11-08 15:38:31 +0700

    chores: env organize

--- components/clients/hello.client.component.tsx
+++ components/clients/hello.client.component.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { NEXT_PUBLIC_APP_NAME } from "@/env";
+
 const HelloClientComponent = () => {
   // test access to env vars in client component
-  const NEXT_PUBLIC_APP_NAME = process.env.NEXT_PUBLIC_APP_NAME;
 
   return <div>Hello from {NEXT_PUBLIC_APP_NAME}</div>;
 };
---
+++ env.ts
@@ -0,0 +1,5 @@
+// server
+export const APP_VERSION = process.env.APP_VERSION ?? "";
+
+// server and client
+export const NEXT_PUBLIC_APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ?? "";
